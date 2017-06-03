01b3ffadffd2950991de8d84f3780562ed936f38
André Bauer <monotek23@gmail.com>
2017-06-03 23:01:53 +0200

    added STACK env var and added repo delete

--- containers/pkgr/docker-entrypoint.sh
+++ containers/pkgr/docker-entrypoint.sh
@@ -8,13 +8,22 @@ if [ "$1" = 'pkgr' ]; then
     sleep 5
   done
 
-  cd ${PKGR_DIR}
+  cd "${PKGR_DIR}"
 
   # checkout repos
+  if [ -d "${PKGR_DIR}/zammad" ]; then
+    rm -r "${PKGR_DIR}/zammad"
+  fi
+
   git clone --depth 1 "${ZAMMAD_URL}"
+
+  if [ -d "${PKGR_DIR}/heroku-buildpack-ruby" ]; then
+    rm -r "${PKGR_DIR}/heroku-buildpack-ruby"
+  fi
+
   git clone --depth 1 "${BUILDPACK_URL}"
 
   # build packages
-  pkgr package --buildpack=${PKGR_DIR}/heroku-buildpack-ruby ${PKGR_DIR}/zammad --env="DATABASE_URL=${DATABASE_URL}"
+  pkgr package --buildpack=${PKGR_DIR}/heroku-buildpack-ruby ${PKGR_DIR}/zammad --env="DATABASE_URL=${DATABASE_URL}" "STACK=heroku-16"
 
 fi
