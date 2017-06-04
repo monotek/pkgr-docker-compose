94815f200a748c300edc5b22f592c6d395870c05
André Bauer <monotek23@gmail.com>
2017-06-04 13:00:16 +0200

    fixed version

--- containers/pkgr/docker-entrypoint.sh
+++ containers/pkgr/docker-entrypoint.sh
@@ -10,19 +10,20 @@ if [ "$1" = 'pkgr' ]; then
 
   # checkout repos
   cd "${PKGR_DIR}"
-  
-  test -d "${PKGR_DIR}/zammad" && rm -r "${PKGR_DIR}/zammad"
-  git clone --depth 1 "${ZAMMAD_URL}"
+
+  test -d "${PKGR_DIR}/${PACKAGE}" && rm -r "${PKGR_DIR}/${PACKAGE}"
+  git clone -b "${PACKAGE_BRANCH}" "${ZAMMAD_URL}"
 
   test -d "${PKGR_DIR}/heroku-buildpack-ruby" && rm -r "${PKGR_DIR}/heroku-buildpack-ruby"
-  git clone --depth 1 "${BUILDPACK_URL}"
+  git clone --depth 1 https://github.com/heroku/heroku-buildpack-ruby
 
   # get version
-  VERSION="$(git -C ${PKGR_DIR}/zammad describe --tags --abbrev=0)"
-  echo "build package for version ${VERSION}"
+  GIT_TAG="$(git -C ${PKGR_DIR}/${PACKAGE} describe --tags --abbrev=0)"
+  VERSION="${GIT_TAG:=0.0.0}"
+  echo "Building ${PACKAGE} package with version ${VERSION}"
   sleep 2
 
   # build packages
-  pkgr package --buildpack=${PKGR_DIR}/heroku-buildpack-ruby ${PKGR_DIR}/zammad --env="DATABASE_URL=${DATABASE_URL}" "STACK=heroku-16" --version=${VERSION:=0.0.0.}
+  pkgr package --buildpack=${PKGR_DIR}/heroku-buildpack-ruby ${PKGR_DIR}/${PACKAGE} --env="DATABASE_URL=${DATABASE_URL}" "STACK=heroku-16" --version=${VERSION}
 
 fi
